5d2f1addaab6fa8088be749b876f66b71a44ef87
br3018 <brbenrose@gmail.com>
2024-10-08 15:22:45 +0100

    Added basic launch file for abp1

---
+++ launch/docklab2_abp1.launch.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Code for launching Air Bearing Platform 1 (ABP1)
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='docklab2',
+            namespace='abp1',
+            executable='service',
+            name='relay_server'
+        )
+    ])
